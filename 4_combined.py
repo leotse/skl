4d28eb0dbd4f18c54bbca6685de4751344d502fc
Leo Tse <leocltse@gmail.com>
2018-09-23 11:04:17 -0400

    Playing around with other classifiers and gridsearch.

--- 4_combined.py
+++ 4_combined.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import FeatureUnion, Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 
 from utils import print_report
 
@@ -56,18 +59,30 @@ pipeline = Pipeline([
     })),
 
     # we now have preprocessed description and pretax features
-    # apply classicifcation model
+    # apply classifier
+    # ('clf', DecisionTreeClassifier()),
+    # ('clf', RandomForestClassifier(n_estimators=100)),
     ('clf', SGDClassifier(
         loss='hinge',
         penalty='l2',
         alpha=1e-3,
-        random_state=42,
         max_iter=5,
         tol=None,
+        random_state=42,
     ))
 ])
 
+# gridsearch loss func and penlaty
+# pipeline = GridSearchCV(pipeline, {
+#     'clf__loss': ['hinge', 'log', 'perceptron'],
+#     'clf__penalty': ['l2', 'l1'],
+#     'clf__alpha': [1e-2, 1e-3, 1e-4, 1e-5],
+#     'clf__max_iter': range(5, 21, 5),
+# }, n_jobs=-1)
+
 clf = pipeline.fit(X_train, y_train)
+# print(f'best score:  {clf.best_score_}')
+# print(f'best params: {clf.best_params_}')
 
 # training data accuracy
 print('=== training error ===')
